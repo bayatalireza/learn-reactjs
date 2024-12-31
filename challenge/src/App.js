79c5d014d2f9c849f7b0d53c2b29087263d49ab9
alireza <bayatalireza86@gmail.com>
2024-12-31 22:54:57 +0330

    `Added currency conversion functionality with API call and state management`

--- challenge/src/App.js
+++ challenge/src/App.js
@@ -1,22 +1,56 @@
-
+import { useEffect, useState } from "react";
 
 function App() {
+  const [amount, setAmount] = useState(1);
+  const [fromCur, setFromCur] = useState("EUR");
+  const [toCur, setToCur] = useState("USD");
+  const [rates, setRates] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function convert() {
+      setIsLoading(true);
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`
+      );
+      const data = await res.json();
+      setRates(data.rates[toCur]);
+      setIsLoading(false);
+    }
+    if (fromCur === toCur) return setRates(amount);
+    convert();
+  }, [amount, fromCur, toCur]);
+
   return (
-    <div >
-      <input type="text" />
-      <select>
+    <div>
+      <input
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+        disabled={isLoading}
+      />
+      <select
+        value={fromCur}
+        onChange={(e) => setFromCur(e.target.value)}
+        disabled={isLoading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select
+        value={toCur}
+        onChange={(e) => setToCur(e.target.value)}
+        disabled={isLoading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>OUTPUT</p>
+      <p>
+        {rates}: {toCur}
+      </p>
     </div>
   );
 }
